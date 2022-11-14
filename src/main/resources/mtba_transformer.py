3a39d973e9a1a32896af9225fabb4a554df63cfb
juarez <d.juarez@dkfz-heidelberg.de>
2022-11-14 10:25:41 +0100

    Mtba Transformer: Hashcode als Observation Id

--- src/main/resources/mtba_transformer.py
+++ src/main/resources/mtba_transformer.py
@@ -3,6 +3,7 @@ import csv
 import re
 import sys
 import uuid
+from hashlib import sha256
 
 Inputfile = sys.argv[1]
 Outputfile = sys.argv[2]
@@ -10,20 +11,21 @@ Outputfile = sys.argv[2]
 def convert_row(headers, row):
     columns=re.split(r'\t+', row[0])
     patient_id=f"{columns[-1]}"
-    diagnosis_id="TODO"
-    specimen_id="TODO"
+    #diagnosis_id="TODO"
+    #specimen_id="TODO"
+    #time_id="TODO"
     mutation_dktk="M"
     mutation_mtba=f"{columns[0]}"
-    identifier=uuid.uuid1()
+    identifier=sha256(''.join(columns).encode('utf-8')).hexdigest()[0:15]
     entry = '<entry>\n' + f'<fullUrl value="http://example.com/Observation/{identifier}" /><resource><Observation xmlns="http://hl7.org/fhir">'
     resource= (f'<id value="{identifier}" />' +
                '<meta><profile value="http://dktk.dkfz.de/fhir/StructureDefinition/onco-core-Observation-GenetischeVariante" /></meta>' +
                '<status value="final" />' +
                '<code><coding><system value="http://loinc.org" /><code value="69548-6" /></coding></code>' +
                f'<subject><reference value="Patient/{patient_id}" /></subject>' +
-               f'<focus><reference value="Condition/{diagnosis_id}"/></focus>' +
-               f'<specimen><reference value="Condition/{specimen_id}"/></specimen>' +
-               #        '<effectiveDateTime value="{time_TODO}" />' +
+               #f'<focus><reference value="Condition/{diagnosis_id}"/></focus>' +
+               #f'<specimen><reference value="Condition/{specimen_id}"/></specimen>' +
+               #f'<effectiveDateTime value="{time_id}" />' +
                '<valueCodeableConcept><coding><system value="http://dktk.dkfz.de/fhir/onco/core/CodeSystem/GenetischeVarianteCS" />' +
                f'<code value="{mutation_dktk}" /></coding></valueCodeableConcept>' +
                '<component><code><coding><system value="http://loinc.org" /><code value="21899-0"/></coding></code>' +
@@ -38,8 +40,8 @@ def convert_row(headers, row):
 with open(Inputfile, 'r') as f:
     r = csv.reader(f)
     headers = next(r)
-    test="a21sd"
-    xml = f'<Bundle xmlns="http://hl7.org/fhir">\n<id value="{test}"/>\n<type value="transaction"/>\n'
+    bundle_id=uuid.uuid1()
+    xml = f'<Bundle xmlns="http://hl7.org/fhir">\n<id value="{bundle_id}"/>\n<type value="transaction"/>\n'
     for row in r:
         xml += convert_row(headers, row) + '\n'
     xml += '</Bundle>'
